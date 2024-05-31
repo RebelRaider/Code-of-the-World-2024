6a46df384f6775b2a63468199071fde71c89eb40
Леонид Чесников <leonid.chesnikov@gmail.com>
2024-05-31 21:55:33 +0300

    bit template changes

--- backend/ml/template_ml_for_api.py
+++ backend/ml/template_ml_for_api.py
@@ -1,19 +1,20 @@
 from ml import interact_hr, interact_hr_with_template
 from llama_cpp import Llama
-
+from parsing.omega_parser import read_any_doc
+import os
 # Шаблон для API
-def template_for_upload_cv(model: Llama, file) -> str:
+def template_for_upload_cv(model: Llama, path_to_temp_file: os.PathLike) -> str:
     """
-    Функция для обработки резюме и вытягивания данных в строку (json???)
+    Функция для обработки резюме и вытягивания данных в строку (json???) (json кста не напиздел)
 
     Args:
         model (Llama):  Модель gguf под llama_cpp
         file (binary?): резюме для распаршивания и отправки в модель
 
     Returns:
-        str: ответ от модели (json???) парсинга резюме по шаблону
+        str: ответ от модели (json???) парсинга резюме по шаблону (json кста не напиздел)
     """
-    resume = "Распаршенное резюме"  # file
+    resume = read_any_doc(path_to_temp_file)
     answer = interact_hr(model=model, content=resume)
     return answer
 
@@ -21,12 +22,12 @@ def template_for_upload_cv(model: Llama, file) -> str:
 # Шаблон для API
 def template_rate_the_candidate(model: Llama, resume_from_db: str, template: str) -> str:
     """
-    шаблон для эндпоинта, который сравнивает резюме и шаблон и возвращает оценку по шаблону
+    шаблон для эндпоинта, который сравнивает резюме и шаблон и возвращает оценку по шаблону (json брат...)
 
     Args:
         model (Llama): Модель gguf под llama_cpp
-        resume_from_db (str): резюме из бдхи конвертированное в строку
-        template (str): шаблон с критериями кандидата
+        resume_from_db (str): резюме из бдхи конвертированное в строку (что хочешь посчитаешь полезным в оценке компетенций то и пихай, наверное навыки и работу конверть, остальное пох)
+        template (str): шаблон с критериями кандидата Максим присылает вместе с id чела чьё резюме мы чекаем (json))))
 
     Returns:
         str: оценка кандидата по шаблону
