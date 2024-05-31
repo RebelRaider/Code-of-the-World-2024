2c4502e8d5330d1670175bacdc14333e95f8f122
svezhest <0088664@mail.ru>
2024-05-31 14:39:31 +0300

    parse odt, other stuff and doc (1997-2003) half of the time

--- backend/parsing/omega_parser.py
+++ backend/parsing/omega_parser.py
@@ -1,54 +1,144 @@
 # Объединение парсеров в один.
 
-# TODO: .doc, .odt
+# TODO: .doc
 # TODO: remove character filtering from pdf_parser (clean_text), because we need latin letters
 
-import re
-from pdf_parser import extract_text_from_pdf, clean_text
+import collections
+from io import StringIO
+import os
 from pathlib import Path
-from docx_parser import DocumentParser # docx_parser
-import docx # python-docx
-from striprtf.striprtf import rtf_to_text # striprtf
+import re
+import xml
+import xml.etree
+import xml.etree.ElementTree
+
+from bs4 import BeautifulSoup as bs
+import docx  # python-docx
+import docx2txt2
+from docx_parser import DocumentParser  # docx_parser
+from markdown import Markdown
+from striprtf.striprtf import rtf_to_text  # striprtf
+import pptx2txt2
+from pydocx import PyDocX
+
+from GigaPdfMiner import extract_text_from_pdf, clean_text
 
 
-def extract_text_from_rtf(file_path: Path) -> str:
-    with open(file_path) as f:
-        text = rtf_to_text( f.read())
+collections.Hashable = collections.abc.Hashable
+
+
+class MarkdownToPlain:
+    def __init__(self):
+        Markdown.output_formats["plain"] = MarkdownToPlain.unmark_element
+        self.md = Markdown(output_format="plain")
+        self.md.stripTopLevelTags = False
+
+    @staticmethod
+    def unmark_element(element: xml.etree.ElementTree.Element, stream: StringIO | None = None) -> str:
+        if stream is None:
+            stream = StringIO()
+        if element.text:
+            stream.write(element.text)
+        for sub in element:
+            MarkdownToPlain.unmark_element(sub, stream)
+        if element.tail:
+            stream.write(element.tail)
+        return stream.getvalue()
+
+    def convert(self, source: str) -> str:
+        return self.md.convert(source)
+
+
+md = MarkdownToPlain()
+
+
+def extract_text_from_markdown(path: os.PathLike) -> str:
+    with open(path) as f:
+        return md.convert(f.read())
+
+
+def extract_text_from_rtf(path: os.PathLike) -> str:
+    with open(path) as f:
+        text = rtf_to_text(f.read())
         text = re.sub(r"\|", "", text)
         return text
 
-def extract_text_from_docx(file_path: Path) -> str:
-    res = []
-    doc = DocumentParser(file_path)
+
+def extract_text_from_docx(path: os.PathLike) -> str:
+    """
+    Извлекает текст из файла формата DOCX (MS OFFICE 2007+).
+    Зачастую бросает ошибку, связанную со структурой документа.
+
+    Parameters:
+    - path (os.PathLike): Путь к файлу.
+
+    Returns:
+    - str: текст элемента
+    """
+    res: list[str] = []
+    doc = DocumentParser(path)
     for _type, item in doc.parse():
-        if 'text' in item:
-            res.append(item['text'])
-        elif 'data' in item:
-            res.extend(' '.join(row) for row in item['data'])
+        if "text" in item:
+            res.append(item["text"])
+        elif "data" in item:
+            res.extend(" ".join(row) for row in item["data"])
 
-    return '\n'.join(res)
+    return "\n".join(res)
 
 
-def extract_text_from_docx_2(file_path: Path) -> str:
-    document = docx.Document(file_path)
-    text = '\n'.join([paragraph.text for paragraph in document.paragraphs])
-    tables = '\n'.join(' '.join(map(str, row.cells))
+def extract_text_from_docx_2(path: os.PathLike) -> str:
+    document = docx.Document(path)
+    text = "\n".join(paragraph.text for paragraph in document.paragraphs)
+    tables = "\n".join(" ".join(map(str, row.cells))
                        for table in document.tables for row in table.rows)
 
-    return f'{text}\n{tables}'
+    return f"{text}\n{tables}"
 
 
-def read_any_doc(path: Path) -> str:
-    extention = path.suffix
-    text = None
+def extract_text_from_doc(path: os.PathLike) -> str:
+    html = PyDocX.to_html(path)
+    return extract_text_with_bs(html)
 
-    if extention == '.pdf':
+
+def extract_text_with_bs(source: str) -> str:
+    soup = bs(source)
+    [s.extract() for s in soup(["style", "script"])]
+    tmpText = soup.get_text(separator=" ")
+    return tmpText
+
+
+def read_any_doc(path: os.PathLike) -> str:
+    text: str = ""
+    extention: str = path.suffix
+
+    if extention == ".txt":
+        with open(path) as f:
+            text = f.read()
+    elif extention == ".md":
+        text = extract_text_from_markdown(path)
+    elif extention == ".pdf":
         text = extract_text_from_pdf(path)
-    elif extention == '.rtf':
+    elif extention == ".rtf":
         text = extract_text_from_rtf(path)
-    else:
+    elif extention == ".pptx" or extention == ".odp":
+        text = pptx2txt2.extract_text(path)
+    elif extention == ".odt":
+        text = docx2txt2.extract_text(path)
+    elif extention == ".docx":
         try:
             text = extract_text_from_docx(path)
         except Exception as e:
-            text = extract_text_from_docx_2(path)
+            try:
+                text = extract_text_from_docx_2(path)
+            except Exception as e:
+                # могут присутствовать артефакты
+                text = docx2txt2.extract_text(path)
+    elif extention == ".doc":
+        try:
+            text = extract_text_from_doc(path)
+        except Exception as e:
+            text = docx2txt2.extract_text(path)
+    else:
+        with open(path, errors="ignore") as f:
+            text = extract_text_with_bs(f.read())
     return clean_text(text)
