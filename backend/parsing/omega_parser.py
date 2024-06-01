7440d5f8bc8f9985e764e667def691ccf1334aab
kirill <shkolenko02-032-2004@mail.ru>
2024-06-01 12:43:07 +0300

    Fixed imports

--- backend/parsing/omega_parser.py
+++ backend/parsing/omega_parser.py
@@ -21,7 +21,7 @@ from striprtf.striprtf import rtf_to_text  # striprtf
 import pptx2txt2
 from pydocx import PyDocX
 
-from pdf_parser import extract_text_from_pdf, clean_text
+from parsing.pdf_parser import extract_text_from_pdf, clean_text
 
 
 collections.Hashable = collections.abc.Hashable
